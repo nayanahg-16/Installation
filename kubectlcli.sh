b38746118b415308d80ca679739ec61c272e74d8
nayanahg-16 <134677278+nayanahg-16@users.noreply.github.com>
2025-12-26 13:16:06 -0500

    Create kubectlcli.sh

---
+++ kubectlcli.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client
+
+curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz
+sudo mv eksctl /usr/local/bin/
+eksctl version
