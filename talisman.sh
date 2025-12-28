6823904ab64413321dce7fbf832cf0c8a7eb9527
nayanahg-16 <134677278+nayanahg-16@users.noreply.github.com>
2025-12-27 23:50:14 -0500

    Update talisman.sh

--- talisman.sh
+++ talisman.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl https://thoughtworks.github.io/talisman/install.sh | bash
+curl https://thoughtworks.github.io/talisman/install.sh >> ~/install-talisman.sh
+chmod +x ~/install-talisman.sh
+sudo yum install perl-Digest-SHA -y
+cd your-git-project
+~/install-talisman.sh
