31e90f818e737968ca1d59da4f55c5266aaece6d
nayanahg-16 <134677278+nayanahg-16@users.noreply.github.com>
2026-01-20 12:46:41 +0530

    Update micrk8s.sh

--- micrk8s.sh
+++ micrk8s.sh
@@ -5,3 +5,16 @@ snap install kubectl -y
 microk8s start
 microk8s config > ~/.kube/config
 alias kubectl="micok8s kubectl"
+
+================================================
+
+for agocd
+
+mkdir -p ~/.kube
+microk8s config > ~/.kube/config
+chmod 600 ~/.kube/config
+kubectl get nodes
+curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.38.0/install.sh | bash -s v0.38.0
+kubectl create -f https://operatorhub.io/install/argocd-operator.yaml
+kubectl get csv -n operators
+
